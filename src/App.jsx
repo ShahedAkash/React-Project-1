37c563b89648b9157a49941b94373ac0cabd162d
ShahedAkash <baker.akash48@gmail.com>
2023-09-16 13:00:12 +0600

    added total price state

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ function App() {
   const [selectedBlogs, setSelectedBlogs] = useState([]);
   const [creditRemainingHours, setCreditRemainingHours] = useState(20);
   const [availableHours, setAvailableHours] = useState(0);
+  const [blogsPrice, setBlogsPrice] = useState(0);
 
   const handleSelectedBlogs = blogs => {
     const isExist = selectedBlogs.find(existId => existId.id === blogs.id);
     let totalHours = blogs.credit;
+    let totalPrice = blogs.price;
     
 
     if(isExist){
@@ -19,6 +21,7 @@ function App() {
     }else{
       selectedBlogs.forEach(time =>{
         totalHours += time.credit;
+        totalPrice += time.price;
       })
 
       const creditHours = creditRemainingHours - blogs.credit;
@@ -27,7 +30,8 @@ function App() {
         return alert("No more Credit hours left") 
       }else{
         setAvailableHours(totalHours);
-        setCreditRemainingHours(creditHours)
+        setCreditRemainingHours(creditHours);
+        setBlogsPrice(totalPrice);
         const forBlogsContainer = [...selectedBlogs, blogs];
         setSelectedBlogs(forBlogsContainer);
       }
@@ -43,7 +47,7 @@ function App() {
       <h1 className=' text-3xl font-bold text-center my-10'>Course Registration</h1>
       <div className='flex gap-4'>
         <Blogs handleSelectedBlogs={handleSelectedBlogs}></Blogs>
-        <MarkedBlogs selectedBlogs={selectedBlogs} availableHours={availableHours} creditRemainingHours={creditRemainingHours}></MarkedBlogs>
+        <MarkedBlogs selectedBlogs={selectedBlogs} availableHours={availableHours} creditRemainingHours={creditRemainingHours} blogsPrice={blogsPrice}></MarkedBlogs>
       </div>
       
     </>
--- src/components/MarkedBlogs/MarkedBlogs.jsx
+++ src/components/MarkedBlogs/MarkedBlogs.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import MarkedBlog from '../MarkedBlog/MarkedBlog';
 
-const MarkedBlogs = ({selectedBlogs, availableHours, creditRemainingHours}) => {
+const MarkedBlogs = ({selectedBlogs, availableHours, creditRemainingHours, blogsPrice}) => {
     return (
         <div className='w-6/12'>
             <h2 className='mb-2 text-xl font-semibold'>Credit Hour Remaining: {creditRemainingHours} hr</h2>
@@ -15,7 +15,9 @@ const MarkedBlogs = ({selectedBlogs, availableHours, creditRemainingHours}) => {
                     ></MarkedBlog>)
             }
             <hr />
-            <h2 className='mt-2 text-xl font-semibold'>Total Credit Hours: {availableHours}</h2>
+            <h2 className='my-2 text-xl font-semibold'>Total Credit Hours: {availableHours}</h2>
+            <hr />
+            <h2 className='mt-2 text-xl font-semibold'>Total Price : {blogsPrice} USD</h2>
         </div>
     );
 };
@@ -23,7 +25,8 @@ const MarkedBlogs = ({selectedBlogs, availableHours, creditRemainingHours}) => {
 MarkedBlogs.propTypes = {
     selectedBlogs: PropTypes.array,
     availableHours: PropTypes.number,
-    creditRemainingHours: PropTypes.number
+    creditRemainingHours: PropTypes.number,
+    blogsPrice: PropTypes.number
 }
 
 export default MarkedBlogs;
